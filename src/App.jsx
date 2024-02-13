e3a09cd329f84e66a01617dd10d337ba01408bc9
arjunsreekumar1882 <arjunsreekumar95@gmail.com>
2024-02-13 10:57:23 +0530

    background-color-change

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+  const handleClickFunction=(e)=>{
+   const body = document.querySelector('body')
+   body.style.background = getRandomColor ()
+  };
+
+  function getRandomColor(){
+    let letters = '0123456789ABCDEF'
+    let color = '#'
+    for(let i=0; i<6; i++){
+      color += letters[Math.floor(Math.random() * 16)]
+      
+    }
+    return color;
+  }
+
+  return (
+    <>
+      <div className='container'>
+        <h1>Background-Color-Change</h1>
+        <button className='btn' onClick={handleClickFunction}>Click Me</button>
+      </div>
+    </>
+  )
+}
+
+export default App
